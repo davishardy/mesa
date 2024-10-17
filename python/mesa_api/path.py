82222c29484e0d5a18b8bfa4829cc73d56a64820
davishardy <davischardy@gmail.com>
2024-10-16 22:18:27 -0400

    Cleaned up formatting of file

--- python/mesa_api/path.py
+++ python/mesa_api/path.py
@@ -8,16 +8,15 @@ class Path:
     """
     def __init__(self):
         """
-        Create Path Object
+        Initialize path object
         """
         self.name = os.getenv("show")
         self.root = os.getenv("show_root")
         self.show_id = self.name[0]
         self.seqs = []
-        # print(f"Show name = {self.name}\nLocation = {self.root}")
+        # print(f"Show name = {self.name}\nLocation = {self.root}")  # Debug
 
 
-    # Get seq path
     def find_sequences(self):
         """
         Finds all sequences in the project and updates the class variable "seqs"
@@ -29,6 +28,8 @@ class Path:
             self.seqs = [seq_dir for seq_dir in os.listdir(self.root) if os.path.isdir(os.path.join(self.root, seq_dir)) and seq_dir.startswith(self.show_id)]
         except ValueError:
             print(f"Current show directory does not exist!\nShow directory = {self.root}")
+        
+        # END find_sequences
 
     def find_shots(self, seq):
         """
@@ -37,10 +38,14 @@ class Path:
         Output = List of shot strings
         """
         # Create path to sequence
-        sequence_dir = os.path.join(self.root, seq)
-        print(sequence_dir)
-        print(os.listdir(sequence_dir))
-        # Get all shots within the sequence ---------------- not working
-        sequence_shots = [shot_dir for shot_dir in os.listdir(sequence_dir) if os.path.isdir(os.path.join(sequence_dir, shot_dir)) and shot_dir.startswith(sequence_dir)]
+        try:
+            sequence_dir = os.path.join(self.root, seq)
+        except ValueError:
+            print("Sequence not present")
+
+        # Get all shots within the sequence
+        sequence_shots = [shot_dir for shot_dir in os.listdir(sequence_dir) if os.path.isdir(os.path.join(sequence_dir, shot_dir)) and shot_dir.startswith(seq)]
         
         return sequence_shots
+    
+        # END find_shots
