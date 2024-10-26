3373c7ae484686c248eba8b75597023b3d384a2d
davishardy <davischardy@gmail.com>
2024-10-26 17:03:56 -0400

    proper env variable setting

--- bin/setshow.sh
+++ bin/setshow.sh
@@ -1,2 +1,2 @@
-export $show=ski
-export $show_root=/Users/davishardy/SCAD/5_Tools/Collaborativespace/ski/show
+export show=ski
+export show_root=/Users/davishardy/SCAD/5_Tools/Collaborativespace/ski/show
