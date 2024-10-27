72fe09d04ae41a52466a22245f76a1b9dbde6bc9
Davis Hardy <davischardy@gmail.com>
2024-10-26 23:34:12 -0400

    simplified creation scripts, WIP

--- python/create_show.py
+++ python/create_show.py
@@ -3,18 +3,6 @@ Create a show with the proper config
 """
 
 import mesa_api
-# Testings path func
-"""
-cur_show = mesa_api.path.Path()
-cur_show.find_sequences()
-print(cur_show.find_shots(cur_show.seqs[0]))
-"""
 
-cur_show_two = mesa_api.Create()
-cur_show_two.show()
-seq_list = ["010", "020", "030", "040", "050", "060", "070"]
-shot_list = ["0010", "0020", "0030", "0040"]
-for seq in seq_list:
-    cur_show_two.sequence(seq)
-    for shot in shot_list:
-        cur_show_two.shot(seq, shot)
+current_show = mesa_api.Create()
+current_show.show()
