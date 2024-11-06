80c9a62aff46693fe8a192e19a0c595555967ba9
davishardy <davischardy@gmail.com>
2024-11-06 12:41:07 -0500

    for local testing

--- bin/setshow.sh
+++ bin/setshow.sh
@@ -1,2 +1,4 @@
-export show=ski
-export show_root=/Users/davishardy/SCAD/5_Tools/Collaborativespace/ski/show
+# Testing script for usage on my laptop
+# Edit per show
+export show=bopeep
+export show_root=/Users/davishardy/SCAD/5_Tools/Collaborativespace/bopeep/show
