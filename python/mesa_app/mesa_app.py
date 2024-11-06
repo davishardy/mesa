b0ad76f56653107fd22355decf19aebfc89586c5
davishardy <davischardy@gmail.com>
2024-11-06 12:41:28 -0500

    gui wip and updated todo list

--- python/mesa_app/mesa_app.py
+++ python/mesa_app/mesa_app.py
@@ -5,31 +5,53 @@ https://help.autodesk.com/view/MAYAUL/2024/ENU/?guid=Maya_SDK_Maya_Python_API_Wo
 https://www.sidefx.com/docs/houdini/ref/windows/pythonpaneleditor.html
 
 """
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir, os.pardir))) # Try to make this less hardcoded
+import mesa_api
 
 # import the Qt framework
 from PySide6.QtCore import QSize, Qt
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # UI setup
         self.setWindowTitle("Mesa")
-
-        self.button_checked = True
-
-        button = QPushButton("First Button")
-        button.setCheckable(True)
-        button.clicked.connect(self.click_action)
-
         self.setMinimumSize(QSize(200, 200))
         self.setMaximumSize(QSize(500, 500))
 
-        self.setCentralWidget(button)
 
-    def click_action(self):
-        print("hi")
+        # Create show button
+        show_button = QPushButton("Create Show")
+        show_button.clicked.connect(self.create_show)
+
+
+        # Placement
+        self.setCentralWidget(show_button)
+
+
+
+    # Button functionality
+    def create_show(self):
+        current_show = mesa_api.Create()
+        current_show.show()
+
+    def create_sequence(self, seq_number):
+        current_show = mesa_api.Create()
+        current_show.sequence(seq_number)
+
+    def create_shot(self, seq_number, shot_number):
+        current_show = mesa_api.Create()
+        current_show.shot(seq_number, shot_number)
+
+
+
+
 
 
 # create app object
